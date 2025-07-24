3f98a173d8330399e9dceeb677c78c1cf2225ffe
vgabriel10 <vittorgabriell420@gmail.com>
2025-07-23 22:10:52 -0300

    Adicionado Behavior para impedir o uso do caracter espaco no nome do usuario

---
+++ CentralSuporte/Behaviors/BloquearEspacoBehavior.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CentralSuporte.Behaviors
+{
+    public class BloquearEspacoBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Contains(" "))
+                e.Handled = true;
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+    }
+}
